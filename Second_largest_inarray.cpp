606ea514cd75969c99a505d20ed70df4a45e5682
Souvik Chowdhury <141899070+Souvik444411@users.noreply.github.com>
2025-05-31 12:45:17 +0530

    Create Second_largest_inarray.cpp

---
+++ Second_largest_inarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        
+        int first = -1;
+        int second = -2;
+        for(int num : arr){
+            if(num>first){
+                second=first;
+                first=num;
+            }
+            else if(num>second && num<first){
+                second = num;
+            }
+        }
+        return second;
+    }
+};
